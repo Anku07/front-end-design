bbd554f4e921d969d3df17836f159eacfac4e766
Anku07 <as19961107@gmail.com>
2024-10-12 20:08:27 +0530

    screen rendering layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
-
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import Section1 from "@/components/main/section1";
+import Section2 from "@/components/main/section2";
+import Section3 from "@/components/main/section3";
+import Section4 from "@/components/main/section4";
+import Section5 from "@/components/main/section5";
+import Section6 from "@/components/main/section6";
+import Section7 from "@/components/main/section7";
+import Section8 from "@/components/main/section8";
 
 export default function Home() {
   return (
     <div>
-      
+      <Header />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+      <Section8 />
+      <Footer />
     </div>
-
   );
 }
