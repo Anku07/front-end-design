f7b5e798227d7d9cf3eba672160b27aa57d27712
Anku07 <as19961107@gmail.com>
2024-10-18 10:08:01 +0530

    creating second part of  section4

--- src/components/main/section4.tsx
+++ src/components/main/section4.tsx
@@ -38,7 +38,33 @@ export default function Section4() {
           </div>
         </div>
 
-
+        {/* Image Grid */}
+        <div className="flex flex-row order-1 md:flex-row gap-1 lg:w-[600px] lg:h-[651px] lg:gap-[16px] w-[361px] h-[309.5.px] justify-between">
+          <div className="flex flex-col lg:w-[172px] w-[94px] lg:h-auto lg:pt-[80px] lg:gap-2 lg:pt-48px pt-12 gap-4px h-281px">
+            <img
+              src="pic1.png"
+              className="w-[72px] h-[96px] rounded-[5px] mx-5 lg:w-[128px] lg:h-[192px] lg:rounded-sm lg:mx-8"
+            />
+            <img src="pic2.png" className="lg:h-[258px] h-[129px]" />
+          </div>
+          <div className="flex flex-col lg:w-[128px] lg:h-auto lg:pt-[32px] lg:gap-2 w-[72px] h-[309px] ">
+            <img src="pic3.png" className="h-[106px] lg:h-[212px]" />
+            <img src="pic5.png" className="h-[107.5px] lg:h-[215px]" />
+            <img src="pic4.png" className="h-[80px] lg:h-[160px]" />
+          </div>
+          <div className="flex flex-col lg:w-[128px] lg:h-auto lg:pt-[45px] lg:gap-2 w-[72px] h-[96px] pt-3">
+            <img src="pic6.png" className="h-[85.5px] lg:h-[172px]" />
+            <img src="pic7.png" className="h-[86px] lg:h-[175px]" />
+            <img src="pic8.png" className="h-[96px] lg:h-[192px]" />
+          </div>
+          <div className="flex flex-col lg:w-[160px] lg:h-auto py-[40px] lg:gap-2 w-[88px] h-[218px] lg:mt-9 ">
+            <img src="pic9.png" className="h-[127.5px ] lg:h-[255px]" />
+            <img
+              src="pic10.png "
+              className="lg:w-[128px] lg:h-[165px] lg:rounded-[5px] h-[82.5px] w-[72px] rounded-[5px]"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
