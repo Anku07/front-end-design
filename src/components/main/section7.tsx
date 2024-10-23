efd80a22d6b9cd8b1aa2c9d0d1fcacae1b31e876
Anku07 <as19961107@gmail.com>
2024-10-23 13:57:07 +0530

    creating sec7 second part

--- src/components/main/section7.tsx
+++ src/components/main/section7.tsx
@@ -38,6 +38,45 @@ export default function Section7() {
             />
           </div>
         </div>
+
+        {/* second details */}
+        <div className="relative lg:w-[600px] lg:h-[479px] w-[361px] h-[421px] mt-4 ">
+          <div className="lg:w-[524px] lg:h-[522px] w-[400px] h-[469px] ">
+            <img src="sec7blob.png" />
+          </div>
+          <div className="absolute lg:w-[200.02px] lg:h-[554px] w-[225.02px] h-[554px] lg:-bottom-10 lg:left-48 -bottom-24 left-48">
+            <img src="sec7icon.png" />
+          </div>
+
+          <div className="absolute lg:bottom-[460px] bottom-[3px] top-4">
+            <div className="absolute flex flex-col max-w-[245px]  max-h-[229px] p-[20px] rounded-[10px] bg-white shadow-md gap-2 bottom">
+              <div className=" flex items-center justify-center w-auto max-w-[65px] h-[24px] px--[8px] py-[8px]  rounded-[4px] bg-[rgba(219,234,254,1)] text-[rgba(30,64,175,1)] ">
+                <span className="text-sm font-medium">Popular</span>
+              </div>
+              <div className="w-[193px] h-[36px] lg:h-[44px]  gap-[4px] mt-5 lg:mt-1">
+                <h3 className="text-base leading-[17.6px]  font-medium">
+                  Design for how people think
+                </h3>
+              </div>
+              <div className="w-[193px] h-[51px]  lg:h-[60px ]">
+                <p className=" text-xs lg:text-sm lg:leading-[19.6px] leading-[16.8px] text-[rgba(71,85,105,1)] font-normal">
+                  Aliquam ut euismod condimentum elementum ultricies volutpat
+                  sit
+                  <br className="2xl:hidden" /> non.
+                </p>
+              </div>
+              <div className="lg:mt-6  lg:w-[190px] lg:h-[72px] w-[225px] h-[72px] ">
+                <button className="  lg:w-[193px] w-[193px] h-[40px] lg:h-[40px] px-10 lg:px-3 lg:py-2 rounded-[8px_8px_8px_8px] border-2 border-[rgba(37,99,235,1)] bg-transparent text-[rgba(37,99,235,1)] text-sm">
+                  <div className="lg:w-[100px] lg:h-[24px] w-[100px] h-[24px] lg:px-2 lg:mx-8">
+                    <span className="lg:w-84px lg:h-[24px] font-medium text-sm leading-6 lg:text-sm">
+                      Take Lesson
+                    </span>
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
