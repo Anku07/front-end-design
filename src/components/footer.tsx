97b4d35429aae6b493c7a9331c4bfd375cb94342
Anku07 <as19961107@gmail.com>
2024-11-05 10:05:45 +0530

    designing footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="bg-[rgba(15,23,42,1)] ">
+        {/* upper part */}
+        <div className=" px-4 sm:px-6 lg:py-3 text-gray-800 sm:grid md:grid-cols-6 sm:grid-cols-2 gap-4  ">
+          <div className="p-5">
+            <h3 className=" flex font-bold text-3xl text-white lg:px-5">
+              <img src="logo2.svg" className="w-[25px]" />
+              teach
+            </h3>
+          </div>
+          <div className="p-5">
+            <div className="text-sm uppercase text-white font-bold">
+              Product
+            </div>
+            <a className="my-3 block text-white" href="/#">
+              Pricing
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Overview
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Browse
+            </a>
+            <a className="flex my-3 gap-2 text-white" href="/#">
+              Accessibility
+              <div className="flex w-[42px] h-[20px] rounded-[4px]  bg-[rgba(243,232,255,1)] justify-center items-center">
+                <div className="flex w-[37px] h-[17px]  gap-[10px] justify-center">
+                  <span className="flex justify-center text-[rgba(107,33,168,1)] text-xs  w-[25px] h-[17px] font-base font-bold  leading-[16.8px]">
+                    BETA
+                  </span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="p-5">
+            <div className="text-sm uppercase text-white font-bold">
+              Product
+            </div>
+            <a className="my-3 block text-white" href="/#">
+              Brainstorming
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Ideation
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Wireframimg
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Research
+            </a>
+          </div>
+
+          <div className="p-5">
+            <div className="text-sm uppercase text-white font-bold">
+              Resources
+            </div>
+            <a className="my-3 block text-white" href="/#">
+              Help Center
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Blog
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Tutorials
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              FAQs
+            </a>
+          </div>
+
+          <div className="p-5">
+            <div className="text-sm uppercase text-white font-bold">
+              Support
+            </div>
+            <a className="my-3 block text-white" href="/#">
+              Contact Us
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Developers
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Documentation
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Integrations
+            </a>
+          </div>
+
+          <div className="p-5">
+            <div className="text-sm uppercase text-white font-bold">
+              Company
+            </div>
+            <a className="my-3 block text-white" href="/#">
+              About
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Press
+            </a>
+            <a className="my-3 block text-white" href="/#">
+              Events
+            </a>
+            <a className="flex my-3 gap-2 text-white" href="/# ">
+              Request Demo
+              <div className="flex justify-center items-center gap-1">
+                <img src="arrow.png" className="mt-1 w-4" />
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
